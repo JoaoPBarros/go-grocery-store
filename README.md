6a0e2f288ebe594e4c5f18e0a34e7cd5f05e1563
JoaoPBarros <joaopedrobarros5@gmail.com>
2022-01-21 11:35:48 -0300

    edit README, registers logic and creatorOfRegister Logic

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # go-grocery-store
+
+First => (I declare the structs and the global variables)
+
+  .then() => (
+    create the function with the logic to describe the each "queue" with a cashier and named as createQtOfRegisters
+  )
+
+  .then() => (
+    create a function to retrieve and execute the selected archieve and until now named as cashier
+  ).catch(func name) => (
+    I may change the name of this function in future to better represent it
+  )
+
+  .then() => (
+    main function to receive the name of archieve and execute the program's logic
+  ).catch(import package) => (
+    for some reason I'm having problems with import my own functions from another files, and for resolve quickly I bundle all together in main.go
+  )
--- main.go
+++ main.go
@@ -9,11 +9,40 @@ import (
 )
 
 type customer struct {
-	itemCount      int16
+	itemCount      int8
 	typeOfCustomer string
 	inService      bool
-	timeArrived    int16
+	timeArrived    int8
 }
+
+type register struct {
+	position      int
+	training      bool
+	arrOfCustomer []customer
+}
+
+var registers = make([]register, 0)
+
+func createQtOfRegisters(x int) {
+	registers := make([]register, x)
+
+	if x != 1 {
+		for i := 0; i < (x - 1); i++ {
+
+			registers[i] = register{
+				position: i + 1,
+				training: false,
+			}
+		}
+	}
+
+	registers[x] = register{
+		position: x,
+		training: true,
+	}
+}
+
+// function to read each file line in root of the project
 
 func cashier(x string) {
 	file, err := os.Open("./" + x + ".txt")
@@ -23,21 +52,25 @@ func cashier(x string) {
 	defer file.Close()
 
 	fileLine := 0
-	numberOfRegisters := 0
 
 	scanner := bufio.NewScanner(file)
+
 	for scanner.Scan() {
+
 		if fileLine != 0 {
 			fileLine = fileLine + 1
+
 			fmt.Println(scanner.Text())
+
 		} else {
 			fileLine = fileLine + 1
+
 			intVar, err := strconv.Atoi(scanner.Text())
 			if err != nil {
 				log.Fatal(err)
 			}
-			numberOfRegisters = intVar
-			fmt.Println(numberOfRegisters)
+
+			createQtOfRegisters(intVar)
 		}
 	}
 
@@ -48,19 +81,15 @@ func cashier(x string) {
 
 // var x customer = customer{ itemCount: 13, typeOfCustomer: "A", inService: true, timeArrived: 2 };
 
-func (x customer) GetItemCount() {
-	fmt.Println(x.itemCount)
-}
-
-// func (x customer)
-
-func lineOfCustomer() {
-
-}
+// func (x customer) GetItemCount() {
+// 	fmt.Println(x.itemCount)
+// }
 
 func main() {
 	var local string
+
 	fmt.Println("qual o nome do arquivo?")
 	fmt.Scan(&local)
 	cashier(local)
+
 }
